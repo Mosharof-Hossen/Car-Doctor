885e06a19fdcf3eee9b5efda4945d0303fc51f9b
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-09-05 01:00:01 +0600

    footer added

--- client/src/Components/Shared/Footer/Footer.jsx
+++ client/src/Components/Shared/Footer/Footer.jsx
@@ -1,9 +1,38 @@
-
+import logo from "../../../assets/logo.svg"
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <footer className="footer bg-[#151515]  text-white p-16 mt-10">
+            <aside>
+                <img src={logo} alt="" className=""/>
+                <p>
+                    Car Doctor Ltd.
+                    <br />
+                    Providing reliable tech since 1992
+                    <br />
+                    developed by <span className="text-primary-c font-semibold">Mosharof Hossen</span>
+                </p>
+            </aside>
+            <nav>
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <h6 className="footer-title">Legal</h6>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+        </footer>
     );
 };
 
