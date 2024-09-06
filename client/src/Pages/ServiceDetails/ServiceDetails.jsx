a4a188516ee5c1992ba05451157e9944bab554f4
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-09-07 00:33:39 +0600

    service details banner done

--- client/src/Pages/ServiceDetails/ServiceDetails.jsx
+++ client/src/Pages/ServiceDetails/ServiceDetails.jsx
@@ -1,27 +1,31 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useLocation } from "react-router-dom";
 
 
 const ServiceDetails = () => {
     const loaderData = useLoaderData()
     const { img } = useLoaderData()
+    const location = useLocation()
+    console.log(location);
     console.log(loaderData);
     return (
         <div>
             <div>
                 <div
-                    className="hero h-[300px] bg-cover bg-center bg-no-repeat "
+                    className="relative hero h-[300px] bg-cover bg-center bg-no-repeat "
                     style={{
                         backgroundImage: `url(${img})`,
                     }}>
                     <div className="hero-overlay bg-opacity-60 "></div>
                     <div className="hero-content text-neutral-content text-center ">
                         <div className="max-w-md">
-                            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                            <p className="mb-5">
-                                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                                quasi. In deleniti eaque aut repudiandae et a id nisi.
-                            </p>
-                            <button className="btn btn-primary">Get Started</button>
+                            <h1 className="mb-5 text-5xl font-bold">Service Details</h1>
+
+                            <button
+                                disabled
+                                style={{ clipPath: "polygon(15% 0%, 85% 0%, 100% 100%, 0% 100%)" }}
+                                className="absolute bottom-0 left-1/2 transform -translate-x-1/2 trapezium bg-primary-c text-white font-bold py-3 px-8 hover:bg-blue-700 transition duration-300">
+                                Home/Service Details
+                            </button>
                         </div>
                     </div>
                 </div>
